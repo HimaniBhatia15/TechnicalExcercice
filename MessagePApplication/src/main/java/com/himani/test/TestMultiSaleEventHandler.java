3c36ac5e5556ce16e6d920170141c8bc6e759fe2
bhatiah <bhatiah@L10778.int.crawley.gov.uk>
2019-08-06 12:33:58 +0100

    Junit Test cases

---
+++ MessagePApplication/src/main/java/com/himani/test/TestMultiSaleEventHandler.java
@@ -0,0 +1,56 @@
+package main.java.com.himani.test;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.com.himani.messageprocessor.exceptions.EventHandlerException;
+import main.java.com.himani.messageprocessor.messagestore.MessageStore;
+import main.java.com.himani.messageprocessor.model.Adjustment;
+import main.java.com.himani.messageprocessor.model.Event;
+import main.java.com.himani.messageprocessor.model.Sale;
+import main.java.com.himani.messageprocessor.model.enums.AdjustmentEnumType;
+import main.java.com.himani.messageprocessor.model.enums.MessageEnumType;
+import main.java.com.himani.messageprocessor.service.impl.MultiSaleEventHandler;
+
+public class TestMultiSaleEventHandler {
+
+	
+	 private MultiSaleEventHandler multiSaleEventHandler;
+	    private List<Sale> sales;
+	    private Sale sale1 = new Sale("Apple", BigDecimal.valueOf(0.50), 1);
+	    private Sale sale2 = new Sale("Orange", BigDecimal.valueOf(0.75), 2);
+
+	    @Before
+	    public void setUp() {
+	        multiSaleEventHandler = new MultiSaleEventHandler();
+	        sales = new ArrayList<>();
+	     MessageStore.salesHistory = sales;
+	    }
+
+	    @Test
+	    public void handleEvent_ok() throws Exception {
+	        Event event = new Event(MessageEnumType.MULTI_SALE, sale2);
+	        multiSaleEventHandler.handleEvent(event);
+	        assertEquals(sale2, MessageStore.salesHistory.get(0));
+	    }
+
+	    @Test(expected = EventHandlerException.class)
+	    public void handleEvent_unit_size_1_should_throw_exception() throws Exception {
+	        Event event = new Event(MessageEnumType.MULTI_SALE, sale1);
+	        multiSaleEventHandler.handleEvent(event);
+	    }
+
+	    @Test(expected = EventHandlerException.class)
+	    public void handleEvent_incorrect_body_type() throws Exception {
+	        Event event = new Event(MessageEnumType.MULTI_SALE, new Adjustment(AdjustmentEnumType.MULTIPLY,
+	                "Apple", BigDecimal.valueOf(2)));
+	        multiSaleEventHandler.handleEvent(event);
+	    }
+
+}
